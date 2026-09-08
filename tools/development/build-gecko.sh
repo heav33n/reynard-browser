630ab474a4b88618b1baa6ee4175e5282cfc5f65
heav33n <rvanfx@gmail.com>
2026-09-07 21:04:41 -0400

    Use empty value to disable Gecko signing option

--- tools/development/build-gecko.sh
+++ tools/development/build-gecko.sh
@@ -49,7 +49,8 @@ fi
 	echo "ac_add_options --enable-optimize"
 	echo "ac_add_options --enable-release"
 	# Release Gecko otherwise ignores xpinstall.signatures.required=false.
-	echo "ac_add_options MOZ_REQUIRE_SIGNING=0"
+	# Gecko boolean environment options use an empty value to disable them.
+	echo "ac_add_options MOZ_REQUIRE_SIGNING="
 	echo "ac_add_options --enable-rust-simd"
 	echo "ac_add_options --enable-lto"
 	echo "ac_add_options --disable-debug"
